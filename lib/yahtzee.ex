945875e7bb8163374703e9dbc2e99538526b7e46
Feray Gulu-zada <fegulu@taltech.ee>
2024-10-03 22:14:12 +0300

    Three of a kind - passing

--- lib/yahtzee.ex
+++ lib/yahtzee.ex
@@ -1,23 +1,26 @@
 defmodule Yahtzee do
-  @moduledoc """
-  Documentation for `Yahtzee`.
-  """
+  def score_lower(dice) when is_list(dice) do
+    # Check if all numbers are positive
+    if Enum.all?(dice, &(&1 > 0)) do
+      counts = count_faces(dice)
 
-  @doc """
-  Hello world.
+      # Check for "Three of a kind"
+      case Enum.find(counts, fn {_face, count} -> count >= 3 end) do
+        {_face, _count} ->
+          # Sum of faces for score
+          score = Enum.sum(dice)
+          %{:"Three of a kind" => score}
 
-  ## Examples
-
-      iex> Yahtzee.hello()
-      :world
-
-  """
-  defmodule Yahtzee do
-    def score_lower(dice) do
-
+        _ ->
+          %{:"Three of a kind" => 0}
       end
+    else
+      %{:"Three of a kind" => 0}  # Return 0 if any dice are not positive
     end
-
-
   end
 
+  defp count_faces(dice) do
+    # Count the occurrences of each face
+    Enum.frequencies(dice)
+  end
+end
